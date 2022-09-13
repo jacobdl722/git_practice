81e783f0423c0b480e47f2c8a1f2a5db12524f5e
jacob <jacobdl3568@gmail.com>
2022-09-13 15:57:41 +0900

    master브랜치에서 파일내용 수정함

--- ex1.py
+++ ex1.py
@@ -2,4 +2,6 @@ def printText(text):
     return f"{text} World!"
 
 text = input().title()
+current_time = int(input("What time is it? "))
 print(printText(text=text))
+print(f"Time of now is {current_time}")
